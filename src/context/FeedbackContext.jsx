ea2d349dbaa33b05a2a9091e4ae9d64c3e70a21a
geep007 <geetparmar001@gmail.com>
2021-12-26 18:12:01 +0530

    adding json server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,31 +1,10 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "Feedback 1",
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: "Feedback 2",
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: "Feedback 3",
-      rating: 6,
-    },
-    {
-      id: 4,
-      text: "Feedback 4",
-      rating: 4,
-    },
-  ]);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},

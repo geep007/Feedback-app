beae719d0d3a3c3caa78b812602945b229affde6
geep007 <geetparmar001@gmail.com>
2021-12-26 15:51:42 +0530

    Refactor app using context api

---
+++ src/context/FeedbackContext.jsx
@@ -0,0 +1,48 @@
+import { createContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: "Feedback 1",
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: "Feedback 2",
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: "Feedback 3",
+      rating: 6,
+    },
+    {
+      id: 4,
+      text: "Feedback 4",
+      rating: 4,
+    },
+  ]);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  return (
+    <FeedbackContext.Provider value={{ feedback, deleteFeedback, addFeedback }}>
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
